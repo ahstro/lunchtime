2e333e3f37b101863d05535f2d24006df6f83876
Anton Strömkvist <anton@lutic.org>
2015-12-02 08:42:02 +0100

    Add settings for types
    Add a checkbox form that lets the user set which types of changes they
    want to see in the stream.

--- js/Store.jsx
+++ js/Store.jsx
@@ -14,10 +14,12 @@ const updateSettings = (setting) => (state, newSetting) => (
 )
 
 const setSources = updateSettings('sources')
+const setTypes = updateSettings('types')
 
 const Store = createStore((state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'SET_SOURCES': return setSources(state, action.newSource)
+    case 'SET_TYPES': return setTypes(state, action.newTypes)
     default: return state
   }
 })
--- js/components/Settings.jsx
+++ js/components/Settings.jsx
@@ -2,19 +2,53 @@ import React from 'react'
 import Store from '../Store'
 
 const Settings = React.createClass({
-  handleChange (e) {
+  getInitialState () {
+    return {
+      types: Store.getState().settings.types
+    }
+  },
+
+  componentDidMount () {
+    Store.subscribe(this.handleSettingsChange)
+  },
+
+  handleSettingsChange () {
+    this.setState({types: Store.getState().settings.types})
+  },
+
+  handleSourcesChange (e) {
     const newSource = Array.from(e.target).filter(target => target.selected)[0].value
-    Store.dispatch({
-      type: 'SET_SOURCES',
-      newSource
-    })
+    Store.dispatch({ type: 'SET_SOURCES', newSource })
+  },
+
+  handleTypesChange (e) {
+    const { types } = this.state
+    const newTypes = types.indexOf(e.target.value) !== -1
+      ? types.filter(type => type !== e.target.value)
+      : types.concat(e.target.value)
+    Store.dispatch({ type: 'SET_TYPES', newTypes })
+  },
+
+  renderCheckboxes () {
+    const { types } = this.state
+    return ['Edit', 'New', 'Log', 'External'] .map(
+      (type, _a, _b, loType = type.toLowerCase()) => (
+        <label>
+          <input
+            checked={types.indexOf(loType) !== -1}
+            value={loType}
+            type='checkbox' />
+          {type}
+        </label>
+      )
+    )
   },
 
   render () {
     // TODO: Change names to correct language
     return (
-      <div>
-        <select onChange={this.handleChange} name='wiki'>
+      <div className='settings'>
+        <select onChange={this.handleSourcesChange} name='wiki'>
           <option value=''>All Sources</option>
           <option value='wikipedia'>Any Wikipedia</option>
           <option value='wikidata'>Wikidata</option>
@@ -22,6 +56,9 @@ const Settings = React.createClass({
           <option value='en.wikipedia'>English Wikipedia</option>
           <option value='sv.wikipedia'>Swedish Wikipedia</option>
         </select>
+        <form onChange={this.handleTypesChange}>
+          {this.renderCheckboxes()}
+        </form>
       </div>
     )
   }
